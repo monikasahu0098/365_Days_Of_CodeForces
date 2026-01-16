5fc292fc9f7645d15dbddfb0ea9892f438576941
Monika Sahu <monikasahu0098@gmail.com>
2026-01-17 01:03:53 +0530

    Add files via upload

---
+++ Stones on the table(266A).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int count=0;
+    
+    for(int i=0;i<s.size()-1;i++){
+        if(s[i]==s[i+1]){
+            count++;
+        }
+    }
+    if(count>0){
+        cout<<count<<endl;
+    }else{
+        cout<<0<<endl;
+    }
+    return 0;
+}
