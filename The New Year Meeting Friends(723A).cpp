f129bc38056c0b601027474cf7777d91d4d51592
Monika Sahu <monikasahu0098@gmail.com>
2026-01-27 23:08:24 +0530

    Add files via upload

---
+++ The New Year Meeting Friends(723A).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n=3;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    sort(v.begin(),v.end());
+    int ans=(v[2]-v[1])+(v[1]-v[0]);
+    cout<<ans<<endl;
+    return 0;
+    
+}
+
