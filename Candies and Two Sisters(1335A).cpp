7d062008e7ca4af9b025fb04fff44fb5d75ad3fd
Monika Sahu <monikasahu0098@gmail.com>
2026-01-29 22:40:53 +0530

    Add files via upload

---
+++ Candies and Two Sisters(1335A).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    
+    while(t--){
+        long long n;
+        cin>>n;
+        long long ans=(n-1)/2;
+        if(ans<0){
+            cout<<0<<endl;
+        }
+        cout<<ans<<endl;
+
+    }
+    return 0;
+}
