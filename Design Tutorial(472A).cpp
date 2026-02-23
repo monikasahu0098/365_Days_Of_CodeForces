035117897f7b4c983439632dcab1cd781ab8e873
Monika Sahu <monikasahu0098@gmail.com>
2026-02-24 00:44:10 +0530

    Add files via upload

---
+++ Design Tutorial(472A).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    long long n;
+    cin>>n;
+    
+    if(n%2==0){
+        cout<<4<<" "<<n-4;
+    }else{
+        cout<<9<<" "<<n-9;
+    }
+    
+    return 0;
+    
+}
