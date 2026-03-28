5ebbeb3427c1faeba2864c9ee2a90ab291858e1f
Monika Sahu <monikasahu0098@gmail.com>
2026-03-28 21:06:58 +0530

    Add files via upload

---
+++ Trippi Troppi(2094A).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s,r,w;
+        cin>>s>>r>>w;
+        
+        cout<<s[0]<<r[0]<<w[0]<<endl;
+        
+    }
+    return 0;
+}
