d10fbab0227f5eeb344a90142509acd1db88762e
Monika Sahu <monikasahu0098@gmail.com>
2026-04-12 21:05:52 +0530

    Add files via upload

---
+++ Make it White(1927A).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        
+        string s;
+        cin>>s;
+        int first = -1, last = -1;
+        
+        for(int i = 0; i < n; i++) {
+            if(s[i] == 'B') {
+                if(first == -1) first = i;
+                last = i;
+            }
+        }
+        
+        cout << (last - first + 1) << "\n";
+    }
+}
