fc757b8c08333afab8f6c8f5da9728b8a13c7077
Monika Sahu <monikasahu0098@gmail.com>
2026-04-25 22:49:37 +0530

    Add files via upload

---
+++ Atilla's Favorite Problem(1760B).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+
+        char mx='a';
+        for(char c:s){
+            if(c>mx){
+                mx=c;
+            }
+        }
+
+        cout<<mx-'a'+1<<"\n";
+    }
+}
