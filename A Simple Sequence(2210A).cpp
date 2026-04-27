e07671175137a10d16ab6b51d6da6f56222b37fa
Monika Sahu <monikasahu0098@gmail.com>
2026-04-27 21:15:01 +0530

    Add files via upload

---
+++ A Simple Sequence(2210A).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        if (n == 2) {
+            cout << "2 1\n";
+            continue;
+        }
+
+        cout << "2 ";
+        
+        for (int i = n; i >= 3; i--) {
+            cout << i << " ";
+        }
+        
+        cout << "1\n";
+    }
+
+    return 0;
+}
