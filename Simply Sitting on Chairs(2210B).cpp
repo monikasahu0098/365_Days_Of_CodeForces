4cf3858a3572245155e710ef8d0b65b4a5ec0269
Monika Sahu <monikasahu0098@gmail.com>
2026-05-04 19:56:17 +0530

    Add files via upload

---
+++ Simply Sitting on Chairs(2210B).cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> p(n + 1);
+        for (int i = 1; i <= n; i++) {
+            cin >> p[i];
+        }
+
+        vector<bool> marked(n + 1, false);
+        int ans = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (marked[i]) break;
+
+            if (p[i] <= i) {
+                ans++;
+                marked[p[i]] = true;
+            }
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
