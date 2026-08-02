9fb761d6a5abe6a467c7431980d751870509c84b
Monika Sahu <monikasahu0098@gmail.com>
2026-08-02 14:29:01 +0530

    Add files via upload

---
+++ A. Make it Beautiful(1783A).cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin>>t;
+ 
+    while(t--){
+        int n;
+        cin>>n;
+ 
+        int a[55];
+ 
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+ 
+        if(a[0]==a[n-1]){
+            cout<<"NO"<<endl;
+        }else{
+            cout<<"YES"<<endl;
+ 
+            cout<<a[n-1]<<" ";
+            for(int i=0;i<n-1;i++)
+                cout<<a[i]<< " ";
+            cout<<endl;
+        }
+    }
+ 
+    return 0;
+}
